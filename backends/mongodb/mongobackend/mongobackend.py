a6f3e1f520c0600732641fb9c64cc34546f7bc0e
atom-based computation database <abcd@gc121mac1.eng.cam.ac.uk>
2015-08-28 17:18:59 +0100

    Corrected a spelling mistake in mongobackend.py

--- backends/mongodb/mongobackend/mongobackend.py
+++ backends/mongodb/mongobackend/mongobackend.py
@@ -5,7 +5,7 @@ __author__ = 'Martin Uhrin'
 import numpy as np
 from pymongo import MongoClient
 from pymongo.son_manipulator import SONManipulator
-from pyongo.objectid import ObjectId
+from pymongo.objectid import ObjectId
 import ase.atoms
 import ase.db.row
 
